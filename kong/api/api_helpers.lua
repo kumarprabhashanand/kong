8780f0aac65e96e39135dc94f2a39dd32382e9a4
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2021-09-10 17:59:08 +0300

    feat(admin) add better support for OPTIONS requests
    ### Summary
    
    Previously Admin API always replied the same on all OPTIONS requests:
    
    ```lua
    header["Access-Control-Allow-Methods"] = "GET, HEAD, PUT, PATCH, POST, DELETE"
    header["Access-Control-Allow-Headers"] = "Content-Type"
    return ngx.exit(204)
    ```
    
    This commit changes that OPTIONS request only replies to routes that our
    Admin API has. Non-existing routes will get `404`.
    
    It also adds `Allow` header to responses, and both `Allow` and
     `Access-Control-Allow-Methods` now contain only the methods that
    the specific API supports.

--- kong/api/api_helpers.lua
+++ kong/api/api_helpers.lua
@@ -18,6 +18,19 @@ local ipairs   = ipairs
 local _M = {}
 local NO_ARRAY_INDEX_MARK = {}
 
+
+local HTTP_METHODS = {
+  ["GET"] = true,
+  ["HEAD"] = true,
+  ["POST"] = true,
+  ["PUT"] = true,
+  ["DELETE"] = true,
+  ["CONNECT"] = true,
+  ["OPTIONS"] = true,
+  ["TRACE"] = true,
+  ["PATCH"] = true,
+}
+
 -- Parses a form value, handling multipart/data values
 -- @param `v` The value object
 -- @return The parsed value
@@ -357,6 +370,17 @@ local function on_error(self)
 end
 
 
+local function options_method(methods)
+  return function()
+    kong.response.exit(204, nil, {
+      ["Allow"] = methods,
+      ["Access-Control-Allow-Methods"] = methods,
+      ["Access-Control-Allow-Headers"] = "Content-Type"
+    })
+  end
+end
+
+
 local handler_helpers = {
   yield_error = app_helpers.yield_error
 }
@@ -366,16 +390,33 @@ function _M.attach_routes(app, routes)
   for route_path, methods in pairs(routes) do
     methods.on_error = methods.on_error or on_error
 
+    local methods_array = {}
+    local methods_count = 0
+
     for method_name, method_handler in pairs(methods) do
       local wrapped_handler = function(self)
         return method_handler(self, {}, handler_helpers)
       end
 
       methods[method_name] = parse_params(wrapped_handler)
+
+      if HTTP_METHODS[method_name] then
+        methods_count = methods_count + 1
+        methods_array[methods_count] = method_name
+      end
     end
 
     if not methods["HEAD"] and methods["GET"] then
       methods["HEAD"] = methods["GET"]
+      methods_count = methods_count + 1
+      methods_array[methods_count] = "HEAD"
+    end
+
+    if not methods["OPTIONS"] then
+      methods_count = methods_count + 1
+      methods_array[methods_count] = "OPTIONS"
+      table.sort(methods_array)
+      methods["OPTIONS"] = options_method(table.concat(methods_array, ", ", 1, methods_count))
     end
 
     app:match(route_path, route_path, app_helpers.respond_to(methods))
@@ -393,6 +434,9 @@ function _M.attach_new_db_routes(app, routes)
 
     methods.on_error = methods.on_error or new_db_on_error
 
+    local methods_array = {}
+    local methods_count = 0
+
     for method_name, method_handler in pairs(methods) do
       local wrapped_handler = function(self)
         self.args = arguments.load({
@@ -404,10 +448,24 @@ function _M.attach_new_db_routes(app, routes)
       end
 
       methods[method_name] = parse_params(wrapped_handler)
+
+      if HTTP_METHODS[method_name] then
+        methods_count = methods_count + 1
+        methods_array[methods_count] = method_name
+      end
     end
 
     if not methods["HEAD"] and methods["GET"] then
       methods["HEAD"] = methods["GET"]
+      methods_count = methods_count + 1
+      methods_array[methods_count] = "HEAD"
+    end
+
+    if not methods["OPTIONS"] then
+      methods_count = methods_count + 1
+      methods_array[methods_count] = "OPTIONS"
+      table.sort(methods_array)
+      methods["OPTIONS"] = options_method(table.concat(methods_array, ", ", 1, methods_count))
     end
 
     app:match(route_path, route_path, app_helpers.respond_to(methods))
